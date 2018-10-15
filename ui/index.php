fbe45526a2e489bbc32c2cecf32c5639eed6c74c
Unknown <t8pito01@students.oamk.fi>
2018-10-15 10:05:19 +0300

    Loputkin osiot kommentoitu

--- ui/index.php
+++ ui/index.php
@@ -1,61 +1,51 @@
 <!DOCTYPE html>
-<html lang="en" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-    <title>Verkkopankki</title>
+<html lang="en" dir="ltr"> <!asetetaan elementtien peruskieleksi englanti ja kirjoitussuunnaksi ltr=letf to right>
+  <head> <!head lohkon sisään tehdään määritykset, sitä ennen html lohkon aloitus, jonka sisään tulee kaikki tieto>
+    <meta charset="utf-8"> <!määritetään html sivun koodaus>
+    <title>Verkkopankki</title> <!määritetään välilehden otsikko>
    
-      <script src="rest.js"></script>
-  </head>
-  <body>
-<div id="login_status">
-  <?php
+      <script src="rest.js"></script> <!sisällytetään script lohkolla rest.js mukaan>
+  </head> <!headlohkon loppu>
+  <body> <!bodylohkon alku>
+
+  <?php //php-koodia html-lohkon sisällä, muutetaan kommentointityyliä
   session_start();
-    if(isset($_SESSION['username'])){
+    if(isset($_SESSION['username'])){ //verrataan käyttäjän antamiin kirjautumistietoihin ja jos ok, toivotetaan tervetulleeksi
       echo 'Tervetuloa '.$_SESSION['username'].'<br>';
-      echo '<a href="../api/logout.php"><button>Kirjaudu ulos</button></a>';
+      echo '<a href="../api/logout.php"><button>Kirjaudu ulos</button></a>'; //määritetään kirjaudu ulos -nappi ja sen toiminta
+      //viittaamaan logout.php tiedostoon
     }
     else {
-      echo 'Tervetuloa vieras ';
+      echo 'Tervetuloa vieras '; //muutoin toivotetaan vieras tervetulleeksi, tämä myös oletuksena sivulle mentäessä tällä hetkellä
       echo '<a href="../ui/login.html"><button>Kirjaudu</button></a>';
-      
+      //Vastavuoroisesti jos kukaan ei ole kirjautunut sisään, asetetaan kirjaudu nappi joka viittaa login.html näkymään
     }
   ?>
-  <hr>
-</div>
 
-<div class="container">
 
-    <p>
+
+
+    <p>  <!tässä napit tilitapahtumille, pankkikorteille sekä maksetuille maksuille
+        viitataan jokaisessa rest.js tiedostossa tehtyyn funktioon<- mitä tapahtuu kun painetaan napista>
         <button class="btn btn-primary" onclick="Tilitapahtumat()">Tilitapahtumat</button>
         <button class="btn btn-primary" onclick="Pankkikortit()">Pankkikortit</button>
         <button class="btn btn-primary" onclick="Maksetutmaksut()">Maksetut maksut</button>
      <p>
         Uusi maksu
         <br>
-        
-        
-        
-        
+        <!luodaan formit uutta maksua varten ja määritellään uusimaksu.php käytettäväksi,
+            annetaan input tyypiksi text ja identifioivat tunnisteet .php tiedostoa varten, jossa arvot sijoitellaan muuttujiin>
         <form action="http://jukkajauhiainen.ipt.oamk.fi/~t8pito01/htyo_verkkopankki/api/uusimaksu.php" method="post">
+            
 Maksun numero: <input type="text" name="mn"> <br>
 Saajan nimi: <input type="text" name="nimi"> <br>
 Viite: <input type="text" name="viite"> <br>
 Saajan Tilinumero: <input type="text" name="st"> <br>
 Summa: <input type="text" name="summa"> <br>
 
-
-<input type="submit">
-</form>
-            
-            
-            
-  
-        
-        
-    
-    
-
-    <div id="results"></div>
-  </div>
-  </body>
-</html>
+<div id="results"></div> <!säiliöidään tulokset myöhemmin käytettäväksi>
+<input type="submit"> <!submit on nimensä mukaisesti lähettäjätyyppinen input type, se käyttäytyy kuin nappi>
+</form> <!formlohko päättyy>
+          
+  </body> <!bodylohko päättyy>
+</html> <!htmllohko päättyy>
--- ui/rest.js
+++ ui/rest.js
@@ -1,14 +1,17 @@
 function Tilitapahtumat() {
   var url = 'http://jukkajauhiainen.ipt.oamk.fi/~t8pito01/htyo_verkkopankki/api/tilitapahtumat.php';
-  var xhttp = new XMLHttpRequest();
-  xhttp.open('GET', url, true);
-  var jsonData = '1';
-  var data = '<table border="10">';
+  var xhttp = new XMLHttpRequest(); //luodaan funktio tilitapahtumat, jossa viittaus tilitapahtumat.php:hen
+  xhttp.open('GET', url, true);//get-metodi
+  var jsonData = '';
+  var data = '<table border="10">'; //määritetään mm. taulukon koko, voisi olla muitakin kuten width jne
   data += '<tr><th>Pvm</th><th>Saaja</th><th>Viite</th><th>Saajan tilinumero</th><th>Selite</th><th>Summa</th></tr>';
-  xhttp.onreadystatechange = function() {
-    if (xhttp.readyState == 4 && xhttp.status == 200) {
+  //yllä asetellaan otsikot
+  //alla mennään funktion if silmukkaan
+  xhttp.onreadystatechange = function() { 
+    if (xhttp.readyState == 4 && xhttp.status == 200) { // readystate == 4 ilmaisee done tilan ja status = 200 tarkoittaa ok-tilaa 
       jsonData = JSON.parse(xhttp.responseText);
-      for (x in jsonData) {
+      for (x in jsonData) { //for silmukalla käydään json tiedostomuodon taulukkoon
+          //mysql-tietokannasta valitut arvot näytille
         data +=
           '<tr><td>' +
           jsonData[x].Pvm +
@@ -21,7 +24,7 @@ function Tilitapahtumat() {
           '</td><td>' +
           jsonData[x].Selite +
           '</td><td>' +
-          jsonData[x].Summa +//muistutuksena: muuttujaei tykkää ä-kirjaimesta sarakkeen nimessä
+          jsonData[x].Summa +//muistutuksena: muuttuja ei tykkää ä-kirjaimesta sarakkeen nimessä
           '</td></tr>'
               
               
@@ -35,12 +38,12 @@ function Tilitapahtumat() {
   xhttp.send();
 }
 
-
+//maksetut maksut ja pankkikortit tehty samalla tyylillä
 function Pankkikortit() {
   var url = 'http://jukkajauhiainen.ipt.oamk.fi/~t8pito01/htyo_verkkopankki/api/pankkikortit.php';
   var xhttp = new XMLHttpRequest();
   xhttp.open('GET', url, true);
-  var jsonData = '1';
+  var jsonData = '';
   var data = '<table border="10">';
   data += '<tr><th>Kortin tyyppi</th><th>Voimassa</th></tr>';
   xhttp.onreadystatechange = function() {
@@ -68,14 +71,14 @@ function Pankkikortit() {
 function Uusimaksu() {
 	var url = 'http://jukkajauhiainen.ipt.oamk.fi/~t8pito01/htyo_verkkopankki/api/uusimaksu.php';
 	var xhttp = new XMLHttpRequest();
-	xhttp.open('POST', url, true);
+	xhttp.open('POST', url, true); //uusimaksu on muuten samalla lailla, php-tiedosto tietysti eroaa
 	var form = document.getElementById('UusiMaksu');
 	var formData = new FormData(form);
 	xhttp.onreadystatechange = function() {
-		if (xhttp.readyState == 4 && xhttp.status == 201) {
-		document.getElementById('results').innerHTML = 'Maksu maksettu!';
+		if (xhttp.readyState == 4 && xhttp.status == 201) { //tässä status = 201 mitä käytetään uuden tiedon luomisessa
+		document.getElementById('results').innerHTML = 'Maksu maksettu!';//tarkastetaan menikö ok
 		} else {
-		document.getElementById('results').innerHTML = 'Jotaki meni pieleen';
+		document.getElementById('results').innerHTML = 'Jotakin meni pieleen';
 		}
 	};
 	xhttp.send(formData);
